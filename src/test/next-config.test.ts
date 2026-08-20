523bad915b517ac9dff28045aa38bf6c68e09197
Davi <davids4@al.insper.edu.br>
2026-08-20 19:54:15 -0300

    Revert "Isola cache local do Next"
    This reverts commit 448b9358ef73a2b7863137d949b43ee97ae0cc5c.

--- src/test/next-config.test.ts
+++
@@ -1,5 +0,0 @@
-import packageJson from "../../package.json";
-
-it("prepares an external cache before starting Next development mode", () => {
-  expect(packageJson.scripts.dev).toBe("node scripts/prepare-dev-cache.mjs && next dev");
-});
